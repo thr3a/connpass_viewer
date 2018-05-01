bc3d927ec566778a522e2933c69b148979c180ff
thr3a <12jk172@ms.dendai.ac.jp>
2018-05-01 11:36:58 +0900

    add: 独自ListView作成

--- app/src/main/java/com/example/thr3a/compassapisample/MainActivity.kt
+++ app/src/main/java/com/example/thr3a/compassapisample/MainActivity.kt
@@ -1,50 +1,85 @@
 package com.example.thr3a.compassapisample
 
+import android.content.Context
 import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ListView
 import android.widget.TextView
 import com.squareup.moshi.KotlinJsonAdapterFactory
 import com.squareup.moshi.Moshi
 import okhttp3.OkHttpClient
 import okhttp3.Request
 
-open class MyAsyncTask : AsyncTask<Void, Void, String>() {
-    override fun doInBackground(vararg p0: Void?): String? {
-        return null
-    }
-
-    override fun onPostExecute(result: String?) {
-        super.onPostExecute(result)
-    }
-}
+//open class MyAsyncTask : AsyncTask<Void, Void, String>() {
+//    override fun doInBackground(vararg p0: Void?): String? {
+//        return null
+//    }
+//
+//    override fun onPostExecute(result: String?) {
+//        super.onPostExecute(result)
+//    }
+//}
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        MyAsyncTask().execute()
+
+        var lv = findViewById(R.id.eventList) as ListView
+
+        var adapter = CustomAdapter(this, arrayListOf(
+                CustomItem("a"),
+                CustomItem("b"),
+                CustomItem("c"),
+                CustomItem("d"),
+                CustomItem("e"),
+                CustomItem("f"),
+                CustomItem("g"),
+                CustomItem("h"),
+                CustomItem("i"),
+                CustomItem("j"),
+                CustomItem("k"),
+                CustomItem("l"),
+                CustomItem("m"),
+                CustomItem("n"),
+                CustomItem("o"),
+                CustomItem("p"),
+                CustomItem("q"),
+                CustomItem("r"),
+                CustomItem("s"),
+                CustomItem("t")
+        ))
+
+        // adapterをset
+        lv.adapter = adapter
+
+//        MyAsyncTask().execute()
     }
 
-    inner class MyAsyncTask: AsyncTask<Void, Void, String>() {
-        override fun doInBackground(vararg p0: Void?): String {
-            return getHtml()
-        }
-
-        override fun onPostExecute(result: String?) {
-            val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
-            val adapter = moshi.adapter(Events::class.java)
-
-            val res = adapter.fromJson(result)
-
-            if (res != null) {
-                val eventCount = res.results_returned.toString()
-                val mytext = findViewById<TextView>(R.id.myText)
-                mytext.setText(eventCount)
-            }
-        }
-    }
+//    inner class MyAsyncTask: AsyncTask<Void, Void, String>() {
+//        override fun doInBackground(vararg p0: Void?): String {
+//            return getHtml()
+//        }
+//
+//        override fun onPostExecute(result: String?) {
+//            val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+//            val adapter = moshi.adapter(Events::class.java)
+//
+//            val res = adapter.fromJson(result)
+//
+//            if (res != null) {
+//                val eventCount = res.results_returned.toString()
+//                val mytext = findViewById<TextView>(R.id.myText)
+//                mytext.setText(eventCount)
+//            }
+//        }
+//    }
 
 }
 
@@ -92,3 +127,48 @@ data class Series(
     val id: Int,
     val title: String
 )
+
+class CustomItem(var text: String) {
+
+}
+
+class CustomAdapter(var context: Context, var items: ArrayList<CustomItem>) : BaseAdapter() {
+    val inflater: LayoutInflater
+
+    init {
+        inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        var v = convertView
+        var holder: CustomViewHolder? = null
+
+        v?.let {
+            holder = it.tag as CustomViewHolder?
+        } ?: run {
+            v = inflater.inflate(R.layout.event_list, null)
+            holder = CustomViewHolder(v?.findViewById(R.id.name) as TextView)
+            v?.tag = holder
+        }
+
+        holder?.let {
+            it.textView.text = items.get(position).text
+        }
+
+        return v as View
+    }
+
+    override fun getItem(position: Int): Any {
+        return items[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    override fun getCount(): Int {
+        return items.size
+    }
+
+    class CustomViewHolder(var textView: TextView)
+}
